0988252dd794f912a0c08c9e159c3a1af3c72f02
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-04-28 14:06:23 -0500

    protected privacy

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.http import Http404
 
 # Create your views here.
 
@@ -12,7 +13,7 @@ def index(request):
 
 @login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
 
     
     context = {'topics':topics}
@@ -23,6 +24,9 @@ def topics(request):
 def topic(request, topic_id):
     topic = Topic.objects.get(id = topic_id)
     entries = topic.entry_set.order_by('-date_added')
+
+    if topic.owner != request.user:
+        raise Http404
 
     context = {'topic':topic, 'entries': entries}
 
@@ -38,7 +42,9 @@ def new_topic(request):
         form = TopicForm(data= request.POST)
         
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             
             return redirect('MainApp:topics')
 
@@ -48,6 +54,10 @@ def new_topic(request):
 @login_required
 def new_entry(request,topic_id):
     topic = Topic.objects.get(id=topic_id)
+
+    if topic.owner != request.user:
+        raise Http404
+        
     if request.method != 'POST':
         form = EntryForm()
     else:
@@ -67,6 +77,9 @@ def new_entry(request,topic_id):
 def edit_entry(request,entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         form = EntryForm(instance=entry)
